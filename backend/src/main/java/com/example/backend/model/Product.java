dba5c5f9341ab1336fc4c658bcc48a8181028d94
Kätlin Teern <katlinteern@gmail.com>
2024-10-13 15:04:43 +0300

    Initial commit for react-product-app

---
+++ backend/src/main/java/com/example/backend/model/Product.java
@@ -0,0 +1,50 @@
+package com.example.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private Double price;
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CreateProductForm from './components/CreateProductForm'
+import NavBar from "./components/NavBar";
+import ProductList from "./components/ProductList/ProductList";
+import Products from "./components/Products";
+import { ProductListProvider } from "./context/ProductContext";
+import ProductDetail from "./components/ProductDetail";
+
+function App() {
+  return (
+    <Router>
+      <ProductListProvider>
+        <div className="container">
+          <NavBar />
+          <hr />
+          <Routes>
+            <Route path="/new" element={<CreateProductForm />} />
+            <Route path="/" element={<Products />}>
+              <Route index element={<ProductList />} />
+              <Route path=":id" element={<ProductDetail />} />
+            </Route>
+          </Routes>
+        </div>
+      </ProductListProvider>
+    </Router>
+  );
+}
+
+export default App;
